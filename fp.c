2ce7a75a03ef29a687cc554512413f475b5f0f04
VarshaPinky <48133025+VarshaPinky@users.noreply.github.com>
2019-09-11 10:14:12 +0530

    Add files via upload

---
+++ fp.c
@@ -0,0 +1,13 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int 
+main(void)
+{
+printf(1,"wow it works\n");
+printf(1,"its :(\n");
+exit();
+}
+
+
---
+++ myprog.c
@@ -0,0 +1,9 @@
+#include "types.h"
+#include "user.h"
+#include "stat.h"
+int 
+main(void){
+printf(1,"My first userprogram in xv6\n");
+exit();
+
+}
